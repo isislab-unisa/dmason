621afab7964da784484c69011b90a726ccf5859a
f.fulgido@gmail.com <f.fulgido@gmail.com@f4fe6fa8-7b7f-eab3-6023-c723d3ff134e>
2012-12-11 10:09:10 +0000

    Added annotation for Thin mode

---
+++ trunk/DMASON/dmason/annotation/Thin.java
@@ -0,0 +1,10 @@
+package dmason.annotation;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Thin {
+
+}
