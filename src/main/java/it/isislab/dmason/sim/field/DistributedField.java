5456562a0d99ebbedb322d876a3902b2ae75434f
miccar <miccar@miccar-OptiPlex-990>
2015-11-17 15:05:50 +0100

    lb methods deleted

--- src/main/java/it/isislab/dmason/sim/field/DistributedField.java
+++ src/main/java/it/isislab/dmason/sim/field/DistributedField.java
@@ -67,52 +67,6 @@ public interface DistributedField<E> extends Serializable
 	
 	//public  UpdateMapNet  getUpdatesNet();
 
-	//Methods for Load Balancing, if you don't need these methods you must "Add Unimplement Method"
-	/**
-	 * Return a list of MyCell
-	 * @return list of cell  
-	 */
-	public HashMap<Integer, MyCellInterface> getToSendForBalance();
-
-	/**
-	 * Set parameter for split
-	 * @param isSplitted true if cell will split   
-	 */
-	public void setIsSplitted(boolean isSplitted);
-	
-	/**
-	 * get parameter on field for split
-	 * @param isSplitted true if cell is splitted   
-	 */
-	public boolean isSplitted();
-	
-	/**
-	 * get parameter on field for split
-	 * @param isPrepareForBalance true if cell is splitting   
-	 */
-	public boolean isPrepareForBalance();
-	
-	/**
-	 * get parameter on field for union
-	 * @param isUnited true if cell is united   
-	 */
-	public boolean isUnited();
-
-	/**
-	 * Set setForBalance 
-	 * @param setForBalance true for loadbalancing
-	 */
-	public void prepareForBalance(boolean prepareForBalance);
-	
-	public HashMap<Integer, MyCellInterface> getToSendForUnion();
-
-
-	public void prepareForUnion(boolean prepareForUnion);
-	
-	/**
-	 * Get number of agents of a region
-	 * @return number of agents of a region
-	 */
 	public int getNumAgents();
 	
 	
