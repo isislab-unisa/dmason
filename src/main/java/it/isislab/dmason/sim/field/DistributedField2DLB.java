30653f97911861584f43633f7b05c17174d5d48e
miccar <miccar@miccar-OptiPlex-990>
2015-11-17 15:06:20 +0100

    new interface for load balancing fields

---
+++ src/main/java/it/isislab/dmason/sim/field/DistributedField2DLB.java
@@ -0,0 +1,56 @@
+package it.isislab.dmason.sim.field;
+
+import it.isislab.dmason.sim.field.support.loadbalancing.MyCellInterface;
+
+import java.util.HashMap;
+
+public interface DistributedField2DLB<E> {
+
+
+	//Methods for Load Balancing, if you don't need these methods you must "Add Unimplement Method"
+	/**
+	 * Return a list of MyCell
+	 * @return list of cell  
+	 */
+	public HashMap<Integer, MyCellInterface> getToSendForBalance();
+
+	/**
+	 * Set parameter for split
+	 * @param isSplitted true if cell will split   
+	 */
+	public void setIsSplitted(boolean isSplitted);
+	
+	/**
+	 * get parameter on field for split
+	 * @param isSplitted true if cell is splitted   
+	 */
+	public boolean isSplitted();
+	
+	/**
+	 * get parameter on field for split
+	 * @param isPrepareForBalance true if cell is splitting   
+	 */
+	public boolean isPrepareForBalance();
+	
+	/**
+	 * get parameter on field for union
+	 * @param isUnited true if cell is united   
+	 */
+	public boolean isUnited();
+
+	/**
+	 * Set setForBalance 
+	 * @param setForBalance true for loadbalancing
+	 */
+	public void prepareForBalance(boolean prepareForBalance);
+	
+	public HashMap<Integer, MyCellInterface> getToSendForUnion();
+
+
+	public void prepareForUnion(boolean prepareForUnion);
+	
+	/**
+	 * Get number of agents of a region
+	 * @return number of agents of a region
+	 */
+}
