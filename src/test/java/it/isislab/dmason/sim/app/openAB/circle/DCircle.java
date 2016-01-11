4555a9da9219a1f4405436c3df0a9ad72031be96
Flavio Serrapica <flavio.serrapica@gmail.com>
2016-01-11 10:10:02 +0100

    Circles simulation by openAB

---
+++ src/test/java/it/isislab/dmason/sim/app/openAB/circle/DCircle.java
@@ -0,0 +1,74 @@
+package it.isislab.dmason.sim.app.openAB.circle;
+
+import java.awt.Color;
+
+import it.isislab.dmason.sim.engine.DistributedState;
+import sim.engine.SimState;
+import sim.util.Bag;
+import sim.util.Double2D;
+
+public class DCircle extends RemoteCircle<Double2D> {
+
+	private static final long serialVersionUID = 1L;
+	protected Color agentColor;
+	double kAttr = 1;
+	double kRep = 1;
+	static double RADIUS = 2;
+	Bag neighbors = null;
+
+	public DCircle(DistributedState<Double2D> sm, Double2D location){
+		super(sm,DCircles.DIAMETER);
+		pos = location;
+		
+	}
+
+	@Override
+	public void step(SimState state) {
+		DCircles st = (DCircles)state;
+		double distance = 0.0;
+		double force = 0.0;
+		double separation_distance = 0.0;
+		double x=0.0, y = 0.0;
+
+		neighbors = st.circles.getNeighborsWithinDistance(pos, 3*RADIUS);
+		for(Object b : neighbors){
+			DCircle other = (DCircle) b;
+			//System.out.println("me "+id+ "and you "+other.id);
+			if(other.id.equalsIgnoreCase(id))
+				continue;
+			distance = euclideanDistance(pos, other.pos);
+			separation_distance = distance - (2*RADIUS);
+			if(separation_distance < RADIUS){
+				if(separation_distance<0)
+					force = kRep;
+				else
+					force = kAttr;
+
+				x +=(force * separation_distance * (other.pos.x - pos.x))/distance;
+
+				y +=(force * separation_distance * (other.pos.y - pos.y))/distance;
+			}
+		}
+		pos = new Double2D(pos.x + x, pos.y +y);
+		st.circles.setObjectLocation(this, pos);
+
+	}
+
+	private double euclideanDistance(Double2D me_location, Double2D other_location){
+
+		return Math.sqrt(Math.pow((me_location.x - other_location.x),2) + Math.pow((me_location.y - other_location.y),2));
+	}
+	
+	public void setColor(Color color) 
+	{
+		this.agentColor = color;
+		paint = agentColor;
+	}
+
+	public Color getColor() {
+		return agentColor;
+	}
+	
+	
+
+}
---
+++ src/test/java/it/isislab/dmason/sim/app/openAB/circle/DCircles.java
@@ -0,0 +1,171 @@
+package it.isislab.dmason.sim.app.openAB.circle;
+
+import java.awt.Color;
+import java.util.List;
+
+import it.isislab.dmason.exception.DMasonException;
+import it.isislab.dmason.experimentals.tools.batch.data.EntryParam;
+import it.isislab.dmason.experimentals.tools.batch.data.GeneralParam;
+import it.isislab.dmason.sim.app.DFlockers.DFlocker;
+import it.isislab.dmason.sim.engine.DistributedMultiSchedule;
+import it.isislab.dmason.sim.engine.DistributedState;
+import it.isislab.dmason.sim.engine.RemotePositionedAgent;
+import it.isislab.dmason.sim.field.DistributedField;
+import it.isislab.dmason.sim.field.continuous.DContinuousGrid2D;
+import it.isislab.dmason.sim.field.continuous.DContinuousGrid2DFactory;
+import sim.engine.SimState;
+import sim.portrayal.SimplePortrayal2D;
+import sim.portrayal.continuous.ContinuousPortrayal2D;
+import sim.portrayal.simple.AdjustablePortrayal2D;
+import sim.portrayal.simple.MovablePortrayal2D;
+import sim.portrayal.simple.OrientedPortrayal2D;
+import sim.util.Double2D;
+
+public class DCircles extends DistributedState<Double2D> {
+
+	public static final double XMIN = 0;
+	public static final double XMAX = 200;
+	public static final double YMIN = 0;
+	public static final double YMAX = 200;
+
+	public static final double DIAMETER = 4;
+
+	public DContinuousGrid2D circles = null;
+
+	public static String topicPrefix = "";
+	
+	public double gridWidth ;
+	public double gridHeight ;   
+	public int MODE;
+	
+	public ContinuousPortrayal2D p;
+	
+	
+	public DCircles(GeneralParam params) {
+		super(params, new DistributedMultiSchedule<Double2D>(),topicPrefix,params.getConnectionType());
+		this.MODE=params.getMode();
+		gridWidth=params.getWidth();
+		gridHeight=params.getHeight();
+	}
+
+	public DCircles(GeneralParam params,List<EntryParam<String, Object>> simParams, String prefix)
+	{
+		super(params,new DistributedMultiSchedule<Double2D>(), prefix,params.getConnectionType());
+		this.MODE=params.getMode();
+		gridWidth=params.getWidth();
+		gridHeight=params.getHeight();
+		topicPrefix = prefix; 
+		for (EntryParam<String, Object> entryParam : simParams) {
+
+			try {
+				this.getClass().getDeclaredField(entryParam.getParamName()).set(this, entryParam.getParamValue());
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchFieldException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		for (EntryParam<String, Object> entryParam : simParams) {
+
+			try {
+				System.out.println(this.getClass().getDeclaredField(entryParam.getParamName()).get(this));
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchFieldException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+	
+	@Override
+	public void start()
+	{
+		super.start();
+		
+		try 
+		{
+			circles = DContinuousGrid2DFactory.createDContinuous2D(8.0,gridWidth, gridHeight,this,
+					super.MAX_DISTANCE,TYPE.pos_i,TYPE.pos_j,super.rows,super.columns,MODE,"circles", topicPrefix,false);
+			init_connection();
+		} catch (DMasonException e) { e.printStackTrace(); }
+		
+		DCircle f=new DCircle(this,new Double2D(0,0));
+		int j=0;
+
+		while(circles.size() != super.NUMAGENTS / super.NUMPEERS)
+		{
+			f.setPos(circles.getAvailableRandomLocation());
+
+			if(circles.setObjectLocation(f, f.pos))
+			{
+				Color c=new Color(
+						128 + this.random.nextInt(128),
+						128 + this.random.nextInt(128),
+						128 + this.random.nextInt(128));
+				f.setColor(c);
+				schedule.scheduleOnce(f);
+				f= new DCircle(this,new Double2D(0,0));
+			}
+
+			j++;
+		}
+		
+	}
+	@Override
+	public DistributedField<Double2D> getField() {
+		// TODO Auto-generated method stub
+		return circles;
+	}
+
+	@Override
+	public void addToField(RemotePositionedAgent<Double2D> rm, Double2D loc) {
+		circles.setObjectLocation(rm, loc);
+		setPortrayalForObject(rm);
+	}
+	
+	public boolean setPortrayalForObject(Object o) 
+	{
+		if(p!=null)
+		{
+			DCircle f=(DCircle)o;
+			SimplePortrayal2D pp = new AdjustablePortrayal2D(new MovablePortrayal2D(new OrientedPortrayal2D(new SimplePortrayal2D(),0,4.0,
+					f.getColor(),
+					OrientedPortrayal2D.SHAPE_COMPASS)));
+			p.setPortrayalForObject(o, pp);
+			return true;
+		}
+		return false;
+	}  
+
+	@Override
+	public SimState getState() {
+		// TODO Auto-generated method stub
+		return this;
+	}
+
+	
+	public static void main(String[] args)
+	{
+		doLoop(DCircles.class, args);
+		System.exit(0);
+	}
+}
