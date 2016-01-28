4bae31aee17643bc3640e2dc781413dfe96d437f
miccar <miccar@localhost.localdomain>
2016-01-28 17:02:17 +0100

    main

--- src/main/java/it/isislab/dmason/experimentals/systemmanagement/master/MasterServerMain.java
+++ src/main/java/it/isislab/dmason/experimentals/systemmanagement/master/MasterServerMain.java
@@ -1,37 +1,44 @@
 package it.isislab.dmason.experimentals.systemmanagement.master;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class MasterServerMain {
 
-	
-	
+
+
 
 	public static void main(String[] args) {
 
-
 		MasterServer master=new MasterServer();
-		master.loadProperties();
 		master.startActivemq();
-		master.createConnection();
-		master.createInitialTopic("Master");
-		
+
 
 		// 1. Creating the server on port 8080
 		Server server = new Server(8080);
+		//ServletContextHandler handler =new ServletContextHandler(server,"resources/systemmanagement/master");
+		ServletContextHandler jarcontext=new ServletContextHandler();
+		jarcontext.setContextPath("/");
+		server.setHandler(jarcontext);
+		jarcontext.addServlet(new ServletHolder(new RunJarServlet("/home/miccar/Scrivania/flockers.jar")),"/execute");
+		
+		
+		//handler.addServletWithMapping(RunJarServlet.class,"/*");
+
 		//server.setHandler(handler);	
 
 
 
 		// 2. Creating the WebAppContext for the created content
-		WebAppContext ctx = new WebAppContext();
-		ctx.setResourceBase("resources/systemmanagement/master");
-		ctx.setContextPath("/master");
+		//WebAppContext ctx = new WebAppContext();
+		//ctx.setResourceBase("resources/systemmanagement/master");
+		//ctx.setContextPath("/master");
 
 
 		//3. Including the JSTL jars for the webapp.
-		ctx.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",".*/[^/]*jstl.*\\.jar$");
+		//ctx.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",".*/[^/]*jstl.*\\.jar$");
 
 		//4. Enabling the Annotation based configuration
 		org.eclipse.jetty.webapp.Configuration.ClassList classlist = org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(server);
@@ -39,7 +46,8 @@ public class MasterServerMain {
 		classlist.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration", "org.eclipse.jetty.annotations.AnnotationConfiguration");
 
 		//5. Setting the handler and starting the Server
-		server.setHandler(ctx);
+
+		//server.setHandler(ctx);
 		try {
 			server.start();
 			server.join();
@@ -47,6 +55,11 @@ public class MasterServerMain {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+
+		//master.excuteSimulationByJar("/home/miccar/Scrivania/flockers.jar");
+
+
 
 	}
 }
