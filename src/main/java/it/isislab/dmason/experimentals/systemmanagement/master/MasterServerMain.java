5764260ed1791618a5c2576401d97eb534f61aaa
miccar <miccar@localhost.localdomain>
2016-02-02 12:01:41 +0100

    update

--- src/main/java/it/isislab/dmason/experimentals/systemmanagement/master/MasterServerMain.java
+++ src/main/java/it/isislab/dmason/experimentals/systemmanagement/master/MasterServerMain.java
@@ -12,26 +12,21 @@ public class MasterServerMain {
 	public static void main(String[] args) {
 
 		MasterServer master=new MasterServer();
-		master.loadProperties();
-		master.startActivemq();
-
-
+		
 		// 1. Creating the server on port 8080
 		Server server = new Server(8080);
 		ServletContextHandler handler =new ServletContextHandler(server,"resources/systemmanagement/master");
 		server.setHandler(handler);	
 
-
-
+		
 		// 2. Creating the WebAppContext for the created content
 		WebAppContext ctx = new WebAppContext();
 		ctx.setResourceBase("resources/systemmanagement/master");
 		ctx.setContextPath("/master");
-
-
+		
 		//3. Including the JSTL jars for the webapp.
 		ctx.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",".*/[^/]*jstl.*\\.jar$");
-
+		
 		//4. Enabling the Annotation based configuration
 		org.eclipse.jetty.webapp.Configuration.ClassList classlist = org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(server);
 		classlist.addAfter("org.eclipse.jetty.webapp.FragmentConfiguration", "org.eclipse.jetty.plus.webapp.EnvConfiguration", "org.eclipse.jetty.plus.webapp.PlusConfiguration");
