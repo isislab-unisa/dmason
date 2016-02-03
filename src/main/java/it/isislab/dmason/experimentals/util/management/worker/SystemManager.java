c2b43e5570b5e01b8bb6d11395702f4ad4c92093
miccar <miccar@localhost.localdomain>
2016-02-03 10:59:32 +0100

    update

--- src/main/java/it/isislab/dmason/experimentals/util/management/worker/SystemManager.java
+++ src/main/java/it/isislab/dmason/experimentals/util/management/worker/SystemManager.java
@@ -18,19 +18,9 @@ package it.isislab.dmason.experimentals.util.management.worker;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
 import java.lang.management.OperatingSystemMXBean;
-import java.lang.management.RuntimeMXBean;
 import java.net.InetAddress;
-import java.text.NumberFormat;
 
-import javax.management.Attribute;
-import javax.management.AttributeList;
-import javax.management.InstanceNotFoundException;
-import javax.management.MBeanServer;
-import javax.management.MalformedObjectNameException;
-import javax.management.ObjectName;
-import javax.management.ReflectionException;
 /**
 * @author Michele Carillo
 * @author Ada Mancuso
@@ -67,57 +57,4 @@ public class SystemManager {
 		return info;
 	}
 
-	
-	
-
-	
-	
-	/*
-	public static void main(String[] args) throws MalformedObjectNameException, InstanceNotFoundException, NullPointerException, ReflectionException {
-		
-		double byte_giga=1073741824;
-		double byte_mega=1048576;
-		
-		Runtime runtime=Runtime.getRuntime();
-		NumberFormat format= NumberFormat.getInstance();
-		
-		
-		long maxMemory = runtime.maxMemory(); //il massimoo della memoria che la vm tenta di usare
-		long allocatedMemory = runtime.totalMemory();//la memoria disponibile per i processi attuali e futuri per la vm
-		long freeMemory = runtime.freeMemory();//la memoria dipsonibili per i futuri oggetti allocati 
-		long used =allocatedMemory-freeMemory;		
-	
-		
-		MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
-		MemoryUsage m= memory.getHeapMemoryUsage();
-		//System.out.println(   Double.parseDouble(""+used)/byte_giga );
-		//System.out.println(   Double.parseDouble(""+m.getUsed())/byte_giga );
-		
-		
-		//MemoryUsage m1=memory.getNonHeapMemoryUsage();
-		
-		
-		System.out.println(Double.parseDouble(""+m.getMax())/byte_giga);
-		
-		
-		//System.out.println(Double.parseDouble(""+freeMemory)/byte_giga);
-		
-	   System.out.println(Double.parseDouble(""+allocatedMemory)/byte_giga);
-		//System.out.println(Double.parseDouble(""+maxMemory)/byte_giga);//m.getmax
-		//System.out.println( Double.parseDouble(""+   ((freeMemory + (maxMemory - allocatedMemory)) )/ byte_giga)   );
-		
-
-		
-		
-		//cpuLOAD
-		//OperatingSystemMXBean  os = ManagementFactory.getOperatingSystemMXBean();
-		//System.out.println("load cpu "+os.getSystemLoadAverage() );
-		
-		//memory available on vm 
-		
-		//memory busy on vm
-		
-		
-	}
-	*/
 }
