c7d463d8f382485e28e879ab848d574b0d1be190
miccar <miccar@localhost.localdomain>
2016-02-19 09:47:14 +0100

    update

--- src/main/java/it/isislab/dmason/experimentals/systemmanagement/master/MasterServerMain.java
+++ src/main/java/it/isislab/dmason/experimentals/systemmanagement/master/MasterServerMain.java
@@ -2,25 +2,20 @@ package it.isislab.dmason.experimentals.systemmanagement.master;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
-import it.isislab.dmason.experimentals.systemmanagement.master.web.utils.GetConnectedWorkersServlet;
 
+
+/**
+ * Main Class for Master Server
+ * 
+ *
+ */
 public class MasterServerMain {
 
 
-     
 
 	public static void main(String[] args){
 
-	/*	MasterServer master =new MasterServer();
-		master.listenonREADY();*/
-		
-		
-		//master.checkAllConnectedWorkers();
-		
-		
-			
 		// 1. Creating the server on port 8080
 		Server server = new Server(8080);
 		ServletContextHandler handler =new ServletContextHandler(server,"resources/systemmanagement/master");	
@@ -30,20 +25,20 @@ public class MasterServerMain {
 		WebAppContext ctx = new WebAppContext();
 		ctx.setResourceBase("resources/systemmanagement/master");
 		ctx.setContextPath("/master");
-		
+
 		//3. Including the JSTL jars for the webapp.
 		ctx.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",".*/[^/]*jstl.*\\.jar$");
-		
+
 		//4. Enabling the Annotation based configuration
 		org.eclipse.jetty.webapp.Configuration.ClassList classlist = org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(server);
 		classlist.addAfter("org.eclipse.jetty.webapp.FragmentConfiguration", "org.eclipse.jetty.plus.webapp.EnvConfiguration", "org.eclipse.jetty.plus.webapp.PlusConfiguration");
 		classlist.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration", "org.eclipse.jetty.annotations.AnnotationConfiguration");
-		
-	//	ctx.addServlet("it.isislab.dmason.experimentals.systemmanagement.master.web.utils.GetConnectedWorkersServlet", "/getWorkers");
-//		ctx.addServlet(new ServletHolder(new GetConnectedWorkersServlet(master)),"/getWorkers");//?name=michele
-//      ctx.addServlet(new ServletHolder(new CreateSimulationFolderServlet(master)), "/createSim"); 
-//      ctx.addBean(master);
-        
+
+		//	ctx.addServlet("it.isislab.dmason.experimentals.systemmanagement.master.web.utils.GetConnectedWorkersServlet", "/getWorkers");
+		//		ctx.addServlet(new ServletHolder(new GetConnectedWorkersServlet(master)),"/getWorkers");//
+		//      ctx.addServlet(new ServletHolder(new CreateSimulationFolderServlet(master)), "/createSim"); 
+		//      ctx.addBean(master);
+
 		//5. Setting the handler and starting the Server
 		server.setHandler(ctx);
 		try {
@@ -55,6 +50,6 @@ public class MasterServerMain {
 
 
 
-	
-}
+
+	}
 }
