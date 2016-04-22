4a80503f11b38fa260e88bc4d489fda743f60f4b
miccar <miccar@micos>
2016-04-22 09:24:09 +0200

    up

--- src/main/java/it/isislab/dmason/experimentals/systemmanagement/master/MasterServer.java
+++ src/main/java/it/isislab/dmason/experimentals/systemmanagement/master/MasterServer.java
@@ -859,7 +859,7 @@ public class MasterServer implements MultiServerInterface{
 			this.getConnection().publishToTopic(iDSimToExec, workerTopic, "start");
 		}
 
-		waitEndSim(idSimulation); 
+		//waitEndSim(idSimulation); 
 	}
 
 
