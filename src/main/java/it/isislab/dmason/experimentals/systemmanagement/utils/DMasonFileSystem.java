17bb384860717160f6c2f6c26654fe3d6065ed1d
miccar <miccar@mikos>
2016-05-19 12:30:42 +0200

    update

--- src/main/java/it/isislab/dmason/experimentals/systemmanagement/utils/DMasonFileSystem.java
+++ src/main/java/it/isislab/dmason/experimentals/systemmanagement/utils/DMasonFileSystem.java
@@ -75,7 +75,7 @@ public class DMasonFileSystem {
 				//check the directory again, if empty then delete it
 				if(file.list().length==0){
 					file.delete();
-					//System.out.println("Directory is deleted : "+ file.getAbsolutePath());
+					System.out.println("Directory is deleted : "+ file.getAbsolutePath());
 				}
 			}
 
