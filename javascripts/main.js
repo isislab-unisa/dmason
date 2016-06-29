f412d9cd030765eda234716e94f2c1c1ec69f54a
Flavio Serrapica <flavio.serrapica@gmail.com>
2016-06-29 11:45:25 +0200

    update dmason site

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,25 @@
 $(function(){window.location="under_construction/pacman-master/index.html"});
-
 /*
+$( function() {
+        var drawer = document.querySelector("paper-drawer-panel");
+
+        drawer.addEventListener("onChange",
+            function (e) {
+                $("#drawer_popup_bookmark").show();
+                drawer.togglePanel();
+                console.log("panel");
+            });
+
+        drawer.addEventListener("iron-select", function (e) {
+            $("#drawer_popup_bookmark").hide();
+            drawer.openDrawer();
+            console.log("select");
+        });
+    }
+);
+
+
 function open_drawer(){
 
     document.querySelector("paper-drawer-panel").openDrawer();
-}
-
-$(
-
-);
-    */
+}*/
