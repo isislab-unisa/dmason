522ff0d2fc8360709fbf1dc5fd84c94a3acc83d1
Flavio Serrapica <flavio.serrapica@gmail.com>
2016-06-30 16:58:45 +0200

    update site

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,5 @@
 
 
- $( function() {
-    var drawer = document.querySelector("paper-drawer-panel");
-
-     drawer.addEventListener("onChange",function (e) {
-         $("#drawer_popup_bookmark").show();
-         drawer.togglePanel();
-         console.log("panel");
-     });
-
-     drawer.addEventListener("iron-select", function (e) {
-         $("#drawer_popup_bookmark").hide();
-         drawer.openDrawer();
-         console.log("select");
-         });
-     }
- );
-
 
  function open_drawer(){
 
