a4d950d40e9c3c7f090ef32e188bb933c79794c1
Flavio Serrapica <flavio.serrapica@gmail.com>
2016-07-04 16:12:40 +0200

    update dmason site

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,15 @@
 function open_drawer(){
   document.querySelector("paper-drawer-panel").togglePanel();
 }
+
+
+function switch_content(element){
+  var id = $(element).attr('href');
+ var a = $(id);
+  console.log(a);
+  $(".content_resized").children().each(function(i){
+    $(this).hide();
+  });
+  console.log($(""+id));
+  $(""+id).css("display","block");
+}
