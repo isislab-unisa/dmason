48f6481dd0abe2045a470e9bf68412a72b57e116
flaser <flavio.serrapica@gmail.com>
2016-07-06 15:07:42 +0200

    update dmason site

--- javascripts/dummy_main.js
+++
@@ -1,1 +0,0 @@
-$(function(){window.location="under_construction/pacman-master/index.html"});
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,19 @@
-function open_drawer(){
+function toggle_drawer(){
   document.querySelector("paper-drawer-panel").togglePanel();
 }
 
 
 function switch_content(element){
   var id = $(element).attr('href');
- var a = $(id);
-  console.log(a);
-  $(".content_resized").children().each(function(i){
-    $(this).hide();
-  });
-  console.log($(""+id));
-  $(""+id).css("display","block");
+
+  if(!id.startsWith("http")) {
+      $(".content_resized").children().each(function (i) {
+          $(this).hide();
+      });
+
+      document.getElementById(id).style.display = "block";
+  }else{
+      window.open(id,"_blank");
+  }
+  toggle_drawer();
 }
