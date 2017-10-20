f1f82895f676ada8795e35d8ae87c72a1c1f16d5
miccar <miccar@micos>
2017-10-20 17:36:42 +0200

    bug fixed

--- src/test/java/it/isislab/dmason/sim/app/DParticles/TestDParticles.java
+++ src/test/java/it/isislab/dmason/sim/app/DParticles/TestDParticles.java
@@ -15,17 +15,18 @@
    limitations under the License.
  */
 package it.isislab.dmason.sim.app.DParticles;
-import it.isislab.dmason.experimentals.tools.batch.data.EntryParam;
-import it.isislab.dmason.experimentals.tools.batch.data.GeneralParam;
 /*
  * THIS CLASS HAS BEEN USED FOR TESTING PURPOSES IN THE BEGINNINGS,
  */
-import it.isislab.dmason.sim.engine.DistributedState;
-import it.isislab.dmason.sim.field.DistributedField2D;
-import it.isislab.dmason.util.connection.ConnectionType;
 
 import java.util.ArrayList;
 
+import it.isislab.dmason.experimentals.systemmanagement.utils.activemq.ActiveMQStarter;
+import it.isislab.dmason.experimentals.tools.batch.data.EntryParam;
+import it.isislab.dmason.experimentals.tools.batch.data.GeneralParam;
+import it.isislab.dmason.sim.engine.DistributedState;
+import it.isislab.dmason.sim.field.DistributedField2D;
+import it.isislab.dmason.util.connection.ConnectionType;
 import sim.display.Console;
 
 /**
@@ -41,68 +42,71 @@ import sim.display.Console;
  */
 public class TestDParticles {
 
-	private static boolean graphicsOn=false; //with or without graphics?
-	private static int numSteps = 3000; //only graphicsOn=false
-	private static int rows = 3; //number of rows
-	private static int columns = 3; //number of columns
-	private static int AOI=1; //max distance
-	private static int NUM_AGENTS=119; //number of agents
-	private static int WIDTH=400; //field width
-	private static int HEIGHT=400; //field height
-	private static int CONNECTION_TYPE=ConnectionType.pureActiveMQ;
-	private static String ip="127.0.0.1"; //ip of activemq
-	private static String port="61616"; //port of activemq
-	private static String topicPrefix="particle"; //unique string to identify topics for this simulation 
-	//don't modify this...
-	private static int MODE = DistributedField2D.UNIFORM_PARTITIONING_MODE;
-	
-	public static void main(String[] args) 
-	{	System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES","*");	
-		class worker extends Thread
-		{
-			private DistributedState ds;
-			public worker(DistributedState ds) {
-				this.ds=ds;
-			}
-			@Override
-			public void run() {
-				ds.start();
-				int i=0;
-				while(i!=numSteps)
-				{
-					//System.out.println(i);
-					ds.schedule.step(ds);
-					i++;
-				}
-			}
-		}
-
-		ArrayList<worker> myWorker = new ArrayList<worker>();
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++) {
-				
-				GeneralParam genParam = new GeneralParam(WIDTH, HEIGHT, AOI, rows, columns, NUM_AGENTS, MODE, CONNECTION_TYPE); 
-				genParam.setI(i);
-				genParam.setJ(j);
-				genParam.setIp(ip);
-				genParam.setPort(port);
-				ArrayList<EntryParam<String, Object>> simParams=new ArrayList<EntryParam<String, Object>>();
-				if(graphicsOn || (i==0 && j==0))
-				{
-					DParticlesWithUI sim =new DParticlesWithUI(genParam,simParams,topicPrefix);
-					((Console)sim.createController()).pressPause();
-				}
-				else
-				{
-					DParticles sim = new DParticles(genParam,simParams,topicPrefix); 
-					worker a = new worker(sim);
-					myWorker.add(a);
-				}
-			}
-		}
-		if(!graphicsOn)
-			for (worker w : myWorker) {
-				w.start();
-			}
-	}
+	private static int numSteps = 300000; //only graphicsOn=false
+    private static int rows = 4; //number of rows
+    private static int columns = 4; //number of columns
+    private static int AOI=10; //max distance
+    private static int NUM_AGENTS=500; //number of agents
+    private static int WIDTH=1200; //field width
+    private static int HEIGHT=1200; //field height
+    private static String ip="127.0.0.1"; //ip of activemq
+    private static String port="61616"; //port of activemq
+    private static String topicPrefix="SIM-NAME"; //unique string to identify topics for this simulation 
+    private static int MODE = DistributedField2D.UNIFORM_PARTITIONING_MODE;
+   
+    
+    public static void main(String[] args) {
+        
+    	/***START EMBEDDED ACTIVEMQ ****/
+    	ActiveMQStarter v=new ActiveMQStarter();
+        v.startActivemq();
+        /******************************/
+        
+        
+        
+    	System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES","*");
+    	class worker extends Thread {
+            		private DistributedState ds;
+            		public worker(DistributedState ds) {
+            			this.ds=ds;
+            			ds.start();
+            		}
+            		public void run() {
+            			int i=0;
+            			while(i!=numSteps)
+            			{
+            				ds.schedule.step(ds);
+            				i++;
+            			}
+            			System.exit(0);
+            		}
+        	}
+        	ArrayList<worker> myWorker = new ArrayList<worker>();
+        	for (int i = 0; i < rows; i++) {
+        		for (int j = 0; j < columns; j++) {
+        			GeneralParam genParam = new GeneralParam(WIDTH, HEIGHT, AOI, rows,columns,NUM_AGENTS, MODE,ConnectionType.pureActiveMQ); 
+        			genParam.setI(i);
+        			genParam.setJ(j);
+        			genParam.setIp(ip);
+        			genParam.setPort(port);
+        			ArrayList<EntryParam<String, Object>> simParams=new ArrayList<EntryParam<String, Object>>();
+        			
+        			if(i==0 && j==0)
+    				{
+    					DParticlesWithUI sim =new DParticlesWithUI(genParam,simParams,topicPrefix);
+    					((Console)sim.createController()).pressPause();
+    				}else {
+    					DParticles sim = new DParticles(genParam,simParams,topicPrefix); 
+            			worker a = new worker(sim);
+            			myWorker.add(a);
+    				}
+        			
+        			
+        			
+        		}
+        	}
+        		for (worker w : myWorker) {
+        			w.start();
+        		}
+        	}
 }
