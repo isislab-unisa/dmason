614c88cdedc6d1a9cf35771855a078468f72fbba
bissim <mauron@live.it>
2017-11-13 11:16:04 +0100

    Remove bower_components from repository Front-end dependencies can be retrieved by giving the 'bower install' command into resources/systemmanagement/master folder.

--- resources/systemmanagement/master/bower_components/polyfill/src/supports.js
+++
@@ -1,9 +0,0 @@
-/**
- * Feature detections
- */
-var supports = {
-  // true with either native support or a polyfil, we don't care which
-  matchMedia: window.matchMedia && window.matchMedia( "only all" ).matches,
-  // true only if the browser supports window.matchMeida natively
-  nativeMatchMedia: isNative(window.matchMedia)
-}
